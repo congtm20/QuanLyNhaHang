16dd8647a2e11f7d4a11ef8e606aadc1e662f1be
Cong Ta <congtm20@gmail.com>
2021-11-08 17:21:41 +0700

    ConnectSQL and Create database QuanLyNhaHang

--- QLNH_APIs/Startup.cs
+++ QLNH_APIs/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using QLNH_APIs.DAO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,11 +34,16 @@ namespace QLNH_APIs
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "QLNH_APIs", Version = "v1" });
             });
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options => options.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader());
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
